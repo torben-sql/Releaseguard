3819b88f69ad5ca6e065332d22370353ccfb50de
torben-sql <torben.n@youmail.dk>
2026-04-16 15:58:25 +0200

    Initial Spring Boot project setup with SQLite and Oracle JDBC dependencies

---
+++ src/main/java/com/releaseguard/ReleaseGuardApplication.java
@@ -0,0 +1,13 @@
+package com.releaseguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReleaseGuardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/releaseguard/ReleaseGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.releaseguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
